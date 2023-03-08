9db58f9ec3c4a891b3e06edf2f35e51aa807406c
Ali Amanvermez <maliamanvermez@gmail.com>
2023-03-09 01:03:02 +0300

    Initial commit

---
+++ iCalories/iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Ali Amanvermez on 9.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
